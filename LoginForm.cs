163d90cec7cb7c253b94ccd51b29acd4c70c27f8
Nikos Dapergolas <nikos.2001@outlook.com.gr>
2022-09-19 19:53:16 +0300

    Different form pops up when a professor or admin logs in

--- LoginForm.cs
+++ LoginForm.cs
@@ -37,6 +37,7 @@ namespace Homework
 
         /// <summary>
         /// The function is called every time to check if a student tries to login
+        /// If at first it sees that this is not a student, it searches if the user trying to login is a professor
         /// </summary>
         void student_login(string username, string password)
         {
@@ -68,7 +69,7 @@ namespace Homework
 
                 // Μηνυμα επιτυχίας και χαράς
                 // και καλώ τη δευτερη φόρμα
-                MessageBox.Show("welcome " + reader.GetString(1) + " " + reader.GetString(2) + "!! :)", "Login Successful");
+                MessageBox.Show("Καλωσόρισες " + reader.GetString(1) + " " + reader.GetString(2) + "!! :)", "Το login ήταν επιτυχές!", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
                 // Πέρασμα στην καινούρια φόρμα με το προφίλ του μαθητή αυτού
                 this.Hide();
@@ -77,10 +78,96 @@ namespace Homework
             }
             else
             {
-                MessageBox.Show("There is no such user...sorry", "ERROR", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
-                // Κώδικας για το αν πατήσει το οκ αντι για cancel
-                // (Σε περίπτωση που χρειαστεί κάπου)
+                professor_login(guna2TextBox1.Text,guna2TextBox2.Text);
+            }
 
+            conn.Close();
+        }
+
+        /// <summary>
+        /// The function is called every time to check if a professor tries to login
+        /// If at first it sees that this is not a professor, it searches if the user trying to login is an admin
+        /// </summary>
+        void professor_login(string username, string password)
+        {
+            // Name of database file
+            string fileName = "HomeworkManagement.db";
+            FileInfo f = new FileInfo(fileName);
+            // Full path to it
+            string path = f.FullName;
+
+            // Connection string with relative path
+            string connectionstring = "Data Source=" + path + ";Version=3;";
+
+            SQLiteConnection conn = new SQLiteConnection(connectionstring);
+            conn.Open();
+            string query1 = "select * from Professor where username='" + username + "' and password='" + password + "';";
+            SQLiteCommand cmd = new SQLiteCommand(query1, conn);
+            SQLiteDataReader reader = cmd.ExecuteReader();
+
+            if (reader.Read())
+            {
+                // Περνάω στο public αντικείμενο Student τις τιμές που αντιστοιχούν σε αυτόν στη βάση
+                // Σύμφωνα με το username και το password του
+                professor = new Professor();
+
+                professor.ID = (int)reader.GetInt32(0);
+                professor.Name = reader.GetString(1);
+                professor.Surname = reader.GetString(2);
+                professor.Email = reader.GetString(3);
+
+                MessageBox.Show("Καλωσόρισες " + reader.GetString(1) + " " + reader.GetString(2) + "!! :)", "Το login ήταν επιτυχές!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Hide();
+                Professor_home_page php = new Professor_home_page();
+                php.Show();
+            }
+            else
+            {
+                admin_login(guna2TextBox1.Text,guna2TextBox2.Text);
+            }
+
+            conn.Close();
+        }
+
+        /// <summary>
+        /// The function is called every time to check if an admin tries to login
+        /// </summary>
+        void admin_login(string username, string password)
+        {
+            // Name of database file
+            string fileName = "HomeworkManagement.db";
+            FileInfo f = new FileInfo(fileName);
+            // Full path to it
+            string path = f.FullName;
+
+            // Connection string with relative path
+            string connectionstring = "Data Source=" + path + ";Version=3;";
+
+            SQLiteConnection conn = new SQLiteConnection(connectionstring);
+            conn.Open();
+            string query1 = "select * from Admin where username='" + username + "' and password='" + password + "';";
+            SQLiteCommand cmd = new SQLiteCommand(query1, conn);
+            SQLiteDataReader reader = cmd.ExecuteReader();
+
+            if (reader.Read())
+            {
+                MessageBox.Show("Καλωσόρισες " + reader.GetString(1) + " " + reader.GetString(2) + "!! :)", "Το login ήταν επιτυχές!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Hide();
+                Admin_home_page ahp = new Admin_home_page();
+                ahp.Show();
+
+                // Περνάω στο public αντικείμενο Student τις τιμές που αντιστοιχούν σε αυτόν στη βάση
+                // Σύμφωνα με το username και το password του
+                admin = new Admin();
+
+                admin.AdminID = (int)reader.GetInt32(0);
+                admin.Name = reader.GetString(1);
+                admin.Surname = reader.GetString(2);
+                admin.Email = reader.GetString(3);
+            }
+            else
+            {
+                MessageBox.Show("Δεν υπάρχει κάποιος χρήστης με το συγκεκριμένο username ή password... Προσπαθήστε ξανά.", "ERROR", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
             }
 
             conn.Close();
